be4d0b54c2135a7112776943b00d211da7428b0f
mihailkalichkov <mihail.kalichkov@gmail.com>
2014-09-09 22:20:31 +0100

    Added Sorting and Searching homework

---
+++ Data Structures and Algorithms/Homework08-SearchingAndSorting/AllTasks/MergeSorter.cs
@@ -0,0 +1,93 @@
+﻿namespace SortingHomework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MergeSorter<T> : ISorter<T> where T : IComparable<T>
+    {
+        public void Sort(IList<T> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("collection", "Collection to sort cannot be null!");
+            }
+
+            IList<T> sortedCollection = this.MergeSort(collection);
+
+            collection.Clear();
+            foreach (T item in sortedCollection)
+            {
+                collection.Add(item);
+            }
+        }
+
+        private IList<T> MergeSort(IList<T> collection)
+        {
+            if (collection.Count <= 1)
+            {
+                return collection;
+            }
+
+            int middle = collection.Count / 2;
+
+            IList<T> left = new List<T>();
+            IList<T> right = new List<T>();
+
+            for (int i = 0; i < middle; i++)
+            {
+                left.Add(collection[i]);
+            }
+
+            for (int i = middle; i < collection.Count; i++)
+            {
+                right.Add(collection[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            return Merge(left, right);
+        }
+
+        private static void AppendListWhileMerge(IList<T> merging, IList<T> merged) //method to append lists after merging
+        {
+            merged.Add(merging[0]);
+            merging.RemoveAt(0);
+        }
+
+        private static IList<T> Merge(IList<T> left, IList<T> right) // method to merge the current list
+        {
+            // generate new list to store the merged lists
+            List<T> merged = new List<T>();
+
+            // compare the values of the list and merge them into a single list called merged
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left[0].CompareTo(right[0]) < 0)
+                    {
+                        AppendListWhileMerge(left, merged);
+                    }
+                    else
+                    {
+                        AppendListWhileMerge(right, merged);
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    AppendListWhileMerge(left, merged);
+                }
+                else if (right.Count > 0)
+                {
+                    AppendListWhileMerge(right, merged);
+                }
+            }
+
+            return merged;
+        }
+    }
+}
---
+++ Data Structures and Algorithms/Homework08-SearchingAndSorting/AllTasks/SortableCollection.cs
@@ -0,0 +1,107 @@
+﻿namespace SortingHomework
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class SortableCollection<T> where T : IComparable<T>
+    {
+        private readonly IList<T> items;
+
+        public SortableCollection()
+        {
+            this.items = new List<T>();
+        }
+
+        public SortableCollection(IEnumerable<T> items)
+        {
+            this.items = new List<T>(items);
+        }
+
+        public IList<T> Items
+        {
+            get
+            {
+                return this.items;
+            }
+        }
+
+        public void Sort(ISorter<T> sorter)
+        {
+            sorter.Sort(this.items);
+        }
+
+        public bool LinearSearch(T item)
+        {
+            foreach (var element in this.items)
+            {
+                if (element.CompareTo(item) == 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool BinarySearch(T item)
+        {
+            this.Sort(new Quicksorter<T>());
+
+            int startIndex = 0;
+            int endIndex = this.items.Count - 1;
+            int searchedIndex = -1;
+
+            while (startIndex <= endIndex)
+            {
+                int middleIndex = (startIndex + endIndex) / 2;
+                if (item.CompareTo(this.items[middleIndex]) < 0)
+                {
+                    endIndex = middleIndex - 1;
+                }
+                else if (item.CompareTo(this.items[middleIndex]) > 0)
+                {
+                    startIndex = middleIndex + 1;
+                }
+                else
+                {
+                    searchedIndex = middleIndex;
+                    break;
+                }
+            }
+
+            return searchedIndex != -1;
+        }
+
+        public void Shuffle()
+        {
+            // http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle 
+            var randomGenerator = new Random();
+
+            for (int i = this.items.Count - 1; i >= 0; i--)
+            {
+                int randomIndex = randomGenerator.Next(0, items.Count - 1);
+
+                T swap = this.items[i];
+                this.items[i] = this.items[randomIndex];
+                this.items[randomIndex] = swap;
+            }
+        }
+
+        public void PrintAllItemsOnConsole()
+        {
+            for (int i = 0; i < this.items.Count; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write(this.items[i]);
+                }
+                else
+                {
+                    Console.Write(" " + this.items[i]);
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
